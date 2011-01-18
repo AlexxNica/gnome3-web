0d08edf7cb76fa207b87d9798cf0bde7e1121070
Jakub Steiner <jimmac@gmail.com>
2011-01-18 23:55:58 +0100

    Push fonts, use a lightbox for screenshots.

---
+++ js/gnome3.js
@@ -0,0 +1,14 @@
+$(document).ready(function () {
+  console.log('foo');
+  $("a[rel='fancybox']").fancybox({
+ 			 'zoomSpeedIn': 200, 
+			 'zoomSpeedOut': 200, 
+			 'overlayShow': true,
+			 'padding': 0,
+			 'overlayShow': true,
+			 'overlayOpacity': 0.8,
+			 'hideOnContentClick': true,
+			 'imageScale': true,
+			 'titlePosition': 'over'
+			 });
+});
