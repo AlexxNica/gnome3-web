91a9709e37fac2dced478d40f7980944f99ab64a
Frédéric Péters <fpeters@0d.be>
2011-02-08 15:07:16 +0100

    [i18n] moved translation build script to po/

---
+++ po/build.sh
@@ -0,0 +1,13 @@
+#! /bin/bash -e
+
+xml2po -e -m xhtml -o gnome3.pot ../*.html
+for POFILE in *.po
+do
+    LANG=$(basename $POFILE .po)
+    for HTMLFILE in ../*.html
+    do
+      echo "Writing $HTMLFILE.$LANG"
+      xml2po -p $POFILE -m xhtml -o ../$HTMLFILE.$LANG $HTMLFILE
+    done
+done
+
