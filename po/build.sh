afcabbac71cb1a58ced46e160c4749f57ed7a8a6
Luca Ferretti <lferrett@gnome.org>
2011-04-01 19:46:14 +0200

    Little trick to fix navigation in localized pages

--- po/build.sh
+++ po/build.sh
@@ -19,6 +19,12 @@ do
         sed -i -e 's/\(<script.*\)/\1<\/script>/' $HTMLFILE.$LANG
       grep -q '<iframe' $HTMLFILE.$LANG && \
         sed -i -e 's/\(<iframe.*\)/\1<\/iframe>/' $HTMLFILE.$LANG
+      grep -q 'index.html\"' $HTMLFILE.$LANG && \
+        sed -i -e "s/index.html\"/index.html.$LANG\"/" $HTMLFILE.$LANG
+      grep -q 'faq.html' $HTMLFILE.$LANG && \
+        sed -i -e "s/faq.html/faq.html.$LANG/" $HTMLFILE.$LANG
+      grep -q 'tryit.html' $HTMLFILE.$LANG && \
+        sed -i -e "s/tryit.html/tryit.html.$LANG/" $HTMLFILE.$LANG
     done
 done
 
