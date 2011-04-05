4217e16771fef086ed0843291ce677eacfe89305
Jakub Steiner <jimmac@gmail.com>
2011-04-05 14:43:13 +0200

    update fancybox to allow gapless sizing of the overlay videos

--- js/gnome3.js
+++ js/gnome3.js
@@ -11,6 +11,19 @@ $(document).ready(function () {
 			 'imageScale': true,
 			 'titlePosition': 'over'
 			 });
+  $("a[rel='fancyvid']").fancybox({
+			 'overlayShow': true,
+			 'padding': 0,
+			 'overlayShow': true,
+			 'overlayOpacity': 0.8,
+			 'overlayColor': '#000000',
+			 'hideOnContentClick': true,
+			 'autoDimensions': false,
+			 'titlePosition': 'over',
+			 'width': 540,
+			 'height': 330,
+			 'showNavArrows': false
+			 });			 
 		$("#splash").click(function () {
 		  $(this).empty()
 		    .html("<iframe width='940' height='405' src='http://www.youtube.com/embed/lepXx1kDelo?autoplay=1&html5=1' frameborder='0' allowfullscreen='true'></iframe>");
