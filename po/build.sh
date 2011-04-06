34e45ff68e4ce3f1dac3f06021b523c996bde922
Frédéric Péters <fpeters@0d.be>
2011-04-06 17:25:10 +0530

    do not let <div class="clear"/> exist

--- po/build.sh
+++ po/build.sh
@@ -19,6 +19,8 @@ do
         sed -i -e 's/\(<script.*\)/\1<\/script>/' $HTMLFILE.$LANG
       grep -q '<iframe' $HTMLFILE.$LANG && \
         sed -i -e 's/\(<iframe.*\)/\1<\/iframe>/' $HTMLFILE.$LANG
+      grep -q '<div class="clear"\/>' $HTMLFILE.$LANG && \
+        sed -i -e 's/<div class="clear"\/>/<div class="clear"><\/div>/' $HTMLFILE.$LANG
       grep -q 'index.html\"' $HTMLFILE.$LANG && \
         sed -i -e "s/index.html\"/index.html.$LANG\"/" $HTMLFILE.$LANG
       grep -q 'faq.html' $HTMLFILE.$LANG && \
